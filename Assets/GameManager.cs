dd893eb0942d299a9c7ed74bdda7b6610bd3229e
kwanGDss <jky6216@naver.com>
2020-06-16 08:46:31 +0900

    Enemy
    Enemy Sound
    Enemy Move
    Enemy Create

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy;
+    private Transform enemyTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateEnermy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateEnermy()
+    {
+        enemy = Instantiate(enemy, new Vector3(-135f, -0.085f, -244f), Quaternion.identity);
+        enemy.name = "ss";
+    }
+}
---
+++ Assets/MonsterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+    private NavMeshAgent navMesh;
+    private GameObject[] randomTransform;   //Dust(s)
+
+    private int random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        randomTransform = GameObject.FindGameObjectsWithTag("Dust");
+
+        random = Random.Range(0, randomTransform.Length);
+
+        navMesh.SetDestination(randomTransform[random].transform.position);
+        //navMesh.SetDestination(playerTransform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, randomTransform[random].transform.position) < 3.0f)
+        {
+            print("Deis");
+
+            random = Random.Range(0, randomTransform.Length);
+
+            navMesh.SetDestination(randomTransform[random].transform.position);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            navMesh.SetDestination(other.gameObject.transform.position);
+
+        print("Collider");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            navMesh.SetDestination(randomTransform[random].transform.position);
+    }
+}
