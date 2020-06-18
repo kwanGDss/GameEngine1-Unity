ce55f08ce17850a6d4810e31829a19bc398e017b
kwanGDss <jky6216@naver.com>
2020-06-18 20:17:34 +0900

    Minimap, SteminaBar
    Add Minimap
    Add SteminaBar

---
+++ Assets/SteminaBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SteminaBar : MonoBehaviour
+{
+    private Slider steminaBar;
+    private Image steminaBarFill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        steminaBar = gameObject.GetComponent<Slider>();
+
+        steminaBarFill = GameObject.Find("Fill").gameObject.GetComponent<Image>();
+        steminaBarFill.color = Color.cyan;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        steminaBar.value = FirstPersonController.stemina;
+
+        if (FirstPersonController.exhaustion)
+            steminaBarFill.color = Color.red;
+        else
+            steminaBarFill.color = Color.cyan;
+    }
+}
